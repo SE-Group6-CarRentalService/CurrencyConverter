5651fa5937f12fefc8af6aa4d209e3cfc685bd20
stefan_pfleger <stefan.pfleger@stud-fh.campuswien.ac.at>
2023-03-21 20:45:42 +0100

    added azure address

--- soap_client.py
+++ soap_client.py
@@ -2,5 +2,7 @@ from suds.client import Client
 from suds.cache import NoCache
 
 my_client = Client('http://127.0.0.1:9000/SOAPservice/soap_service/?WSDL', cache=NoCache())
+#currency-converter-group6.azurewebsites.net
+
 print(my_client)
 print('output: ', my_client.service.convertCurrency('EUR',50,'JPY'))
