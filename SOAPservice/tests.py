8663e1bef20dce3931374b71d239f3cdc883886c
leonard <leonardrolandvictor@googlemail.com>
2023-04-01 13:12:04 +0200

    neues requirements file

--- SOAPservice/tests.py
+++ SOAPservice/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+
+class SOAPserviceTest(SimpleTestCase):
+
+        def test_SOAPservice_statuscode(self):
+                response = self.client.get('/SOAPservice/soap_service/?WSDL')
+                self.assertEquals(response.status_code,200)
+                                
 
 # Create your tests here.
