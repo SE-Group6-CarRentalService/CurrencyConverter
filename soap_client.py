bf44578341bff93cd24441c51d652e981fb212d0
Stefan <92309004+OtakuStef@users.noreply.github.com>
2023-04-30 20:28:41 +0200

    Update soap_client.py
    Added AWS IP

--- soap_client.py
+++ soap_client.py
@@ -1,7 +1,7 @@
 from suds.client import Client
 from suds.cache import NoCache
 
-#my_client = Client('http://127.0.0.1:8000/SOAPservice/soap_service/?WSDL', cache=NoCache())
+#my_client = Client('http://13.50.231.174:8501/SOAPservice/soap_service/?WSDL', cache=NoCache())
 my_client = Client('https://currency-converter-g6.azurewebsites.net/SOAPservice/soap_service/?WSDL', cache=NoCache())
 
 print(my_client)
